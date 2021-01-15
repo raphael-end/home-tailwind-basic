a46aadcdd4ff5a50c1a598507f1bd2caa4eeb6bd
Raphael de moura Teixeira <raphaeldemourateixeira@MacBook-Pro-de-Raphael.local>
2021-01-14 22:46:25 -0300

    commited resources

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# home-tailwind-basic
-Home page made with tailwind css
+<h1>LAYOUT</h1>
+--------------eng--------------
+<h1>Home page made with tailwind css</h1>
+<p>This is a simple home page made with tailwind css for a laravel project</p>
+
+--------------pt--------------
+<h1> Página inicial feita com css tailwind </h1>
+<p> Esta é uma página inicial simples feita com css tailwind para um projeto laravel </p>
+
+--------------eng--------------
+<h1>Usage:</h1>
+<p>start php artisan serve, to start the laravel project</p>
+<p>read the tailwind css documentation -> tailwindcss.com</p>
+<p>edit the class and be happy!</p>
+--------------pt--------------
+<h1> Uso: </h1>
+<p> iniciar o artesão php serve, para iniciar o projeto laravel </p>
+<p> leia a documentação do css do tailwind -> tailwindcss.com </p>
+<p>Edite as classes e seja feliz!</p>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home - Investidor Moderno</title>
+
+    <link rel="icon" type="image/png" href="{{ asset("images/favicon_32x32.png") }}"/>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">   
+
+</head>
+<body>
+<header class="text-gray-400 body-font" style="position: sticky; width:100%; height:800px; background-image: url(https://demo.phlox.pro/bitcoin-elementor/wp-content/uploads/sites/74/2018/05/header.jpg); background-repeat: no-repeat;
+    background-size: cover;">
+  <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+    <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+      <img class="rounded" src="{{ asset("images/favicon_32x32.png") }}">
+      <span class="ml-3 text-gray-400 text-xl">Investidor Moderno</span>
+    </a>
+    <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
+      <a class="mr-5 hover:text-white">Home</a>
+      <a class="mr-5 hover:text-white">Sobre</a>
+      <a class="mr-5 hover:text-white">Curso</a>
+    </nav>
+    <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">LIVE TRADING
+      <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
+        <path d="M5 12h14M12 5l7 7-7 7"></path>
+      </svg>
+    </button>
+  </div>
+  <section class="text-gray-500 body-font">
+  <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+    <div class="lg:flex-grow md:w-1/2 lg:pr-24 lg:mt-20 md:pr-16 flex flex-col  md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+      <h1 class="lg:text-7xl title-font sm:text-4xl text-3xl mb-4 font-extrabold text-white">RENTABILIZE
+      </h1>
+      <p class="mb-8 leading-relaxed font-semibold lg:text-3xl">SEU CAPITAL APRENDENDO TUDO <br>
+                                                                SOBRE O BITCOIN E OS INVESTIMENTOS <br>
+                                                                DA NOVA ECONOMIA.
+      </p>
+      <div class="flex justify-center">
+        <button class="inline-flex text-white bg-gradient-to-r from-yellow-400 to-yellow-600 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">CONHEÇA MAIS</button>
+      </div>
+    </div>
+    <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+      
+    </div>
+  </div>
+</section>
+</header>
+<!-- MAIN -->
+<section class="text-gray-600 body-font">
+  <div class="container px-5 py-24 mx-auto">
+    <div class="lg:grid grid-cols-2 gap-3">
+      <div class="p-4 ">
+        <div class="h-full bg-gray-100 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative  shadow-xl">
+         <img  class="tracking-widest px-64 mb-2" src="{{ asset("images/logbt.png") }}">
+          <h1 class="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">LIVE TRADING da BeloTrade</h1>
+          <p class="leading-relaxed mb-3">Além da análise técnica do 0 a Trader, com a Investidor Moderno, você vai se formar um Especialista em Análise de Sentimento de Mercado; especialidade conhecida por poucos Traders Seniores no mundo e que faz toda a diferença ao operar o criptomercado.</p>
+          <a class="text-yellow-500 inline-flex items-center">Saiba mais
+            <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+              <path d="M5 12h14"></path>
+              <path d="M12 5l7 7-7 7"></path>
+            </svg>
+          </a>
+          <div class="text-center mt-2 leading-none flex justify-center absolute bottom-0 left-0 w-full py-4">
+            
+          </div>
+        </div>
+      </div>
+      <div class="p-4">
+        <div class="h-full bg-gray-100 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative  shadow-xl">
+          <img  class="tracking-widest px-64 mb-2" src="{{ asset("images/logbt.png") }}">
+          <h1 class="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">Conheça o nosso curso</h1>
+          <p class="leading-relaxed mb-3">Além da análise técnica do 0 a Trader, com a Investidor Moderno, você vai se formar um Especialista em Análise de Sentimento de Mercado; especialidade conhecida por poucos Traders Seniores no mundo e que faz toda a diferença ao operar o criptomercado.</p>
+          <a class="text-yellow-500 inline-flex items-center">Quero fazer o curso
+            <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+              <path d="M5 12h14"></path>
+              <path d="M12 5l7 7-7 7"></path>
+            </svg>
+          </a>
+          <div class="text-center mt-2 leading-none flex justify-center absolute bottom-0 left-0 w-full py-4">
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- BANNER2 -->
+  <div class="text-center relative bg-black m-100% h-96" style=" background-image: url('{{ asset("images/banner2.png") }}'); background-position: center;">
+    <h1 class="lg:pt-28 title-font sm:text-2xl text-xl font-medium text-gray-300 pt-24 py-10">A NOVA ECONOMIA É REVOLUÇÃO, LIBERDADE, <br>
+                                                                                      DE SCENTRALIZAÇÃO… VOCÊ ESTÁ PREPARADO?
+            </h1>
+    <div class="flex justify-center">
+      <button class="inline-flex text-white bg-transparent border-2 border-white py-2 px-6 focus:outline-none hover:bg-yellow-600 hover:bg-opacity-25 hover:border-yellow-600 hover:text-yellow-600 rounded text-lg">CONHEÇA MAIS</button>
+    </div>
+  </div>
+  <section class="mt-28 text-gray-600 body-font">
+    <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+      <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+        <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">capitais em um modelo de <br>
+          negócio descentralizado onde <br>
+          você mantém a custódia de <br>
+          seus ativos.
+        </h1>
+        <div class="flex justify-center">
+          <button class="inline-flex text-white bg-gradient-to-r from-yellow-400 to-yellow-600  border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">COMECE AGORA</button>
+        </div>
+      </div>
+      <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+        <img class="object-cover object-center rounded" alt="hero" src="{{ asset("images/bolabt.png")}}" >
+      </div>
+    </div>
+  </section>
+
+</section>
+<!-- MAIN -->
+<footer class="w-100% h-40 mt-28" style="background-color: #101010;">
+
+</footer>
+</body>
+</html>
